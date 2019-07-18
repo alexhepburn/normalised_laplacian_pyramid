6e2110ddaed2d01516b3421e4698254858035835
Alex hepburn <hepburnalexander@gmail.com>
2019-07-18 11:23:18 +0100

    Create nlp.py

---
+++ nlp.py
@@ -0,0 +1,160 @@
+"""
+Normalised Laplacian Pyramid implemeneted in Pytorch
+Author: Alex Hepburn <ah13558@bristol.ac.uk>
+"""
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+import numpy as np
+
+from gdn import GDN
+
+
+LAPLACIAN_FILTER = np.array([[0.0025, 0.0125, 0.0200, 0.0125, 0.0025],
+                             [0.0125, 0.0625, 0.1000, 0.0625, 0.0125],
+                             [0.0200, 0.1000, 0.1600, 0.1000, 0.0200],
+                             [0.0125, 0.0625, 0.1000, 0.0625, 0.0125],
+                             [0.0025, 0.0125, 0.0200, 0.0125, 0.0025]],
+                             dtype=np.float32)
+
+class lap_pyramid(nn.Module):
+    """
+    Normalised lapalcian pyramid implmeneted with filters learnt from
+    the McGill natural image dataset.
+    """
+    def __init__(self, k, dims=3, filt=None):
+        super(lap_pyramid, self).__init__()
+        if filt is None:
+            filt = np.reshape(np.tile(LAPLACIAN_FILTER, (dims, 1, 1)),
+                              (dims, 1, 5, 5))
+        self.k = k
+        self.dims = dims
+        self.filt = nn.Parameter(torch.Tensor(filt), requires_grad=False)
+        self.dn_filts, self.sigmas = self.DN_filters()
+        self.pad_one = nn.ReflectionPad2d(1)
+        self.pad_two = nn.ReflectionPad2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear',
+                                    align_corners=True)
+
+    def DN_filters(self):
+        sigmas = [0.0248, 0.0185, 0.0179, 0.0191, 0.0220, 0.2782]
+        dn_filts = []
+        dn_filts.append(torch.Tensor(np.reshape([[0, 0.1011, 0],
+                                    [0.1493, 0, 0.1460],
+                                    [0, 0.1015, 0.]]*self.dims,
+                                   (self.dims,	1, 3, 3)).astype(np.float32)))
+
+        dn_filts.append(torch.Tensor(np.reshape([[0, 0.0757, 0],
+                                    [0.1986, 0, 0.1846],
+                                    [0, 0.0837, 0]]*self.dims,
+                                   (self.dims, 1, 3, 3)).astype(np.float32)))
+
+        dn_filts.append(torch.Tensor(np.reshape([[0, 0.0477, 0],
+                                    [0.2138, 0, 0.2243],
+                                    [0, 0.0467, 0]]*self.dims,
+                                   (self.dims, 1, 3, 3)).astype(np.float32)))
+
+        dn_filts.append(torch.Tensor(np.reshape([[0, 0, 0],
+                                    [0.2503, 0, 0.2616],
+                                    [0, 0, 0]]*self.dims,
+                                   (self.dims, 1, 3, 3)).astype(np.float32)))
+
+        dn_filts.append(torch.Tensor(np.reshape([[0, 0, 0],
+                                    [0.2598, 0, 0.2552],
+                                    [0, 0, 0]]*self.dims,
+                                   (self.dims, 1, 3, 3)).astype(np.float32)))
+
+        dn_filts.append(torch.Tensor(np.reshape([[0, 0, 0],
+                                    [0.2215, 0, 0.0717],
+                                    [0, 0, 0]]*self.dims,
+                                   (self.dims, 1, 3, 3)).astype(np.float32)))
+        dn_filts = nn.ParameterList([nn.Parameter(x, requires_grad=self.trainable)
+                                     for x in dn_filts])
+        sigmas = nn.ParameterList([nn.Parameter(torch.Tensor(np.array(x)),
+                                  requires_grad=self.trainable) for x in sigmas])
+        return dn_filts, sigmas
+
+    def pyramid(self, im):
+        out = []
+        J = im
+        pyr = []
+        for i in range(0, self.k):
+            I = F.conv2d(self.pad_two(J), self.filt, stride=2, padding=0,
+                         groups=self.dims)
+            I_up = self.upsample(I)
+            I_up_conv = F.conv2d(self.pad_two(I_up), self.filt, stride=1,
+                                 padding=0, groups=self.dims)
+            if J.size() != I_up_conv.size():
+                I_up_conv = torch.nn.functional.interpolate(I_up_conv, [J.size(2), J.size(3)])
+            out = J - I_up_conv
+            out_conv = F.conv2d(self.pad_one(torch.abs(out)), self.dn_filts[i],
+                         stride=1, groups=self.dims)
+            out_norm = out / (self.sigmas[i]+out_conv)
+            pyr.append(out_norm)
+            J = I
+        return pyr
+
+    def compare(self, x1, x2):
+        y1 = self.pyramid(x1)
+        y2 = self.pyramid(x2)
+        total = []
+        # Calculate difference in perceptual space (Tensors are stored
+        # strangley to avoid needing to pad tensors)
+        for z1, z2 in zip(y1, y2):
+            diff = (z1 - z2) ** 2
+            sqrt = torch.sqrt(torch.mean(diff, (1, 2, 3)))
+            total.append(sqrt)
+        return torch.mean(torch.stack(total), dim=0)
+
+
+class lap_pyramid_GDN(nn.Module):
+    """
+    Trainable normalised laplacian pyramid with GDN modules to do the
+    local divisive normalisation.
+    """
+    def __init__(self, k, dims=3, filt=None):
+        super(lap_pyramid_GDN, self).__init__()
+        if filt is None:
+            filt = np.tile(LAPLACIAN_FILTER, (dims, 1, 1))
+            filt = np.reshape(np.tile(LAPLACIAN_FILTER, (dims, 1, 1)),
+                              (dims, 1, 5, 5))
+        self.k = k
+        self.dims = dims
+        self.filt = nn.Parameter(torch.Tensor(filt))
+        self.filt.requires_grad = False
+        self.gdns = nn.ModuleList([GDN(dims, apply_independently=True) for i in range(self.k)])
+        self.pad_one = nn.ReflectionPad2d(1)
+        self.pad_two = nn.ReflectionPad2d(2)
+        self.mse = nn.MSELoss(reduction='none')
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear',
+                                    align_corners=True)
+
+    def pyramid(self, im):
+        J = im
+        pyr = []
+        for i in range(0, self.k):
+            I = F.conv2d(self.pad_two(J), self.filt, stride=2, padding=0,
+                         groups=self.dims)
+            I_up = self.upsample(I)
+            I_up_conv = F.conv2d(self.pad_two(I_up), self.filt, stride=1,
+                                 padding=0, groups=self.dims)
+            if J.size() != I_up_conv.size():
+                I_up_conv = torch.nn.functional.interpolate(I_up_conv, [J.size(2), J.size(3)])
+            pyr.append(self.gdns[i](J - I_up_conv))
+            J = I
+        return pyr
+
+    def compare(self, x1, x2):
+        y1 = self.pyramid(x1)
+        y2 = self.pyramid(x2)
+        total = []
+        # Calculate difference in perceptual space (Tensors are stored
+        # strangley to avoid needing to pad tensors)
+        for z1, z2 in zip(y1, y2):
+            diff = (z1 - z2) ** 2
+            sqrt = torch.sqrt(torch.mean(diff, (1, 2, 3)))
+            total.append(sqrt)
+        return torch.mean(torch.stack(total), dim=0)
